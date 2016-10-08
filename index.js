3b083b3b5d8c2c961a715d7798334a6ea6bc33b1
Wolfgang Merkt <wxmerkt@users.noreply.github.com>
2016-10-08 17:47:29 +0100

    Less verbose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Buffer = require('buffer').Buffer;
 var Mailgun = {
 	sendEmail: function(params, options){
 		var url = 'https://api.mailgun.net/v2/'+Mailgun.domainName+'/messages'
-		console.log(url);
+		// console.log(url);
 		var p = new Parse.Promise();
 		var resolved;
 		var successCallback = function(httpResponse){
